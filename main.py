ce8fcb1703a6a61597a80452212db53dc3cbf287
JustinLong56 <66042565+JustinLong56@users.noreply.github.com>
2022-12-29 14:49:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+from discord.ext import commands
+import os
+
+#import all of the cogs
+from music_cog import music_cog
+
+
+#place your bot token here for the bot to read
+token = " "
+
+#command prefix for bot
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
+#adds bot
+async def load_extensions():
+    await bot.add_cog(music_cog(bot))
+ 
+#have bot read token           
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(token)
+
+#runs bot
+asyncio.run(main())
+
